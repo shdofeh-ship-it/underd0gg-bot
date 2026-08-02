9de3747b0cd0c92ea129e16ab31a4848462a8e98
shdofeh-ship-it <shdofeh@gmail.com>
2026-08-02 06:28:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,13 +11,10 @@ from database import init_db
 # Импорт роутеров (пока подключаем только start)
 from handlers.start import router as start_router
 from handlers.menu import router as menu_router
-
+from handlers.profile import router as profile_router
 async def main():
     # Логирование
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(message)s"
-    )
+    logging.basicConfig(...)
 
     # Создаем бота
     bot = Bot(
@@ -30,16 +27,16 @@ async def main():
     # Создаем диспетчер
     dp = Dispatcher()
 
-    # Подключаем базу данных
+    # Подключаем базу
     await init_db()
 
     # Подключаем роутеры
     dp.include_router(start_router)
-dp.include_router(menu_router)
+    dp.include_router(menu_router)
+    dp.include_router(profile_router)
 
     print("UNDERD0GG BOT ONLINE")
 
-    # Запуск
     await dp.start_polling(bot)
 
 
