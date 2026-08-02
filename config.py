abc6aa18586d7a5ef5b91fdde10668df14454b54
shdofeh-ship-it <shdofeh@gmail.com>
2026-08-02 14:12:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Токен бота
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "8980798606:AAHAnaLYx75-0FeuuaHUBywcIt7E5wKETJs"
 # ID администраторов (через запятую)
 ADMIN_IDS = [
     1053853724    
