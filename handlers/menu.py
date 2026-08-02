ef0abbc142ef573f3dcd2bfc353fa5d3d13587ca
shdofeh-ship-it <shdofeh@gmail.com>
2026-08-02 06:42:45 +0300

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,33 +1,78 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram import F
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+
+from keyboards import main_menu, back_keyboard
+from config import KICK_URL
 
 router = Router()
 
 
-@router.message(F.text == "🔴 Статус стрима")
-async def stream_status(message: Message):
-    await message.answer(
-        "🔴 <b>STREAM STATUS</b>\n\n"
-        "SYSTEM: ONLINE\n\n"
-        "Kick:\n"
-        "https://kick.com/underd0gg"
+@router.callback_query(F.data == "back")
+async def back(callback: CallbackQuery):
+    text = """
+<pre>
+██████████████████████
+
+      UNDERD0GG
+
+No Face.
+No Limits.
+
+██████████████████████
+
+SYSTEM STATUS:
+ONLINE
+
+Nobody Knows.
+Everybody Watches.
+</pre>
+"""
+
+    await callback.message.edit_text(
+        text,
+        reply_markup=main_menu()
     )
 
 
-@router.message(F.text == "⚙️ Настройки")
-async def settings(message: Message):
-    await message.answer(
-        "⚙️ Настройки будут доступны в следующем обновлении."
+@router.callback_query(F.data == "status")
+async def status(callback: CallbackQuery):
+    await callback.message.edit_text(
+        f"""
+<b>🔴 STREAM STATUS</b>
+
+STATUS:
+🟢 ONLINE
+
+🎥 Kick:
+{KICK_URL}
+""",
+        reply_markup=back_keyboard()
     )
 
 
-@router.message(F.text == "🔗 Ресурсы")
-async def resources(message: Message):
-    await message.answer(
-        "🌐 UNDERD0GG NETWORK\n\n"
-        "Kick\n"
-        "YouTube\n"
-        "TikTok\n\n"
-        "Ссылки добавим позже."
+@router.callback_query(F.data == "resources")
+async def resources(callback: CallbackQuery):
+    await callback.message.edit_text(
+        """
+<b>🌐 UNDERD0GG NETWORK</b>
+
+🎥 Kick
+▶ YouTube Shorts
+🎵 TikTok
+
+Ссылки будут добавлены позже.
+""",
+        reply_markup=back_keyboard()
+    )
+
+
+@router.callback_query(F.data == "settings")
+async def settings(callback: CallbackQuery):
+    await callback.message.edit_text(
+        """
+<b>⚙ SETTINGS</b>
+
+Раздел находится в разработке.
+""",
+        reply_markup=back_keyboard()
     )
